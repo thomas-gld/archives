795dbe6deca453edd882a2bd4099cda009b48b6e
thomas_g <thomas.gladin.pro@gmail.com>
2025-12-13 09:15:13 +0100

    Add login and register views

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 import express from 'express'
 import 'dotenv/config'
+import prisma from './prisma.js'
 
 const app = express()
 
+const db = await prisma.User.findMany()
+console.log(db)
+
 app.set('view engine', "ejs")
+
+app.use('/asset', express.static('asset'))
+
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register')
 })
 
 const port = process.env.PORT
